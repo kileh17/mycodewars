12711dff9ba6dc7bde5ee813597c71b75a321157
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-05-31 22:50:43 -0400

    7th solution

---
+++ js/solution007.js
@@ -0,0 +1,6 @@
+let array = [ 0, 1, 2, 3, 4 ]
+function toCsv(array){
+    return array.join('\n');
+}
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
