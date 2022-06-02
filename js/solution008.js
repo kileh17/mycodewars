788c462549ea4b11d1ea831a02e65933548934dd
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-06-01 23:37:23 -0400

    8th solution

---
+++ js/solution008.js
@@ -0,0 +1,9 @@
+function even_or_odd(number) {
+    let answer = ""
+     if(number % 2 == 0){
+       return "Even"
+     }else{
+       return "Odd"
+     };
+  }
+//   Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
