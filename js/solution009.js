48ab19b11448cce38920201dfc39455e9e3d4800
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-06-02 21:42:32 -0400

    9th solution

---
+++ js/solution009.js
@@ -0,0 +1,4 @@
+function plural(n){
+    return n === 1 ? false : true
+}
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
