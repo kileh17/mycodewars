9f155faa1d80cb6030978434a6ab41cb47397ddb
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-06-06 21:06:10 -0400

    12 solution

---
+++ js/solution012.js
@@ -0,0 +1,5 @@
+const x = "john McClane"
+const words = x.split(' ')
+let revName = words[1] +  ' ' +  words[0]
+console.log(revName)
+// Write a function that returns a string in which firstname is swapped with last name.
