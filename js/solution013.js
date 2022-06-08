a86d6ea190cf5e29b3d8702e548149ceb9b56e63
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-06-07 21:25:23 -0400

    13th solution

---
+++ js/solution013.js
@@ -0,0 +1,14 @@
+let numbers = [1,2,3,4,5,6]
+function squareSum(numbers){
+    let cubeNumbers = []
+    let result = 0
+    numbers.forEach( function(element,index){
+      cubeNumbers[index] = Math.pow(element,2)
+    })
+    result = cubeNumbers.reduce((previous,current) => previous + current,0)
+    return result
+  }
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
