b001331433881e79af4455e63aeb518e80f851ae
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-06-10 22:54:28 -0400

    summing an array problem

---
+++ js/solution015.js
@@ -0,0 +1,9 @@
+const nums = [1, 5.2, 4, 0, -1]
+let sum = 0
+nums.forEach(element => {
+    sum += element
+});
+
+// put that into a function and it works
+
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
