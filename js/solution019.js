aa98528cbd4e45c989757614f6542e1a0a1aeba9
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-06-14 23:20:22 -0400

    convert string to number

---
+++ js/solution019.js
@@ -0,0 +1,7 @@
+let str1 = "2"
+const cleanStr = Number(str1)
+console.log(cleanStr)
+
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
