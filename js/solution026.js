7c15b4e4693f0ca220fcaf870f5554e7070a11a7
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-06-22 22:21:18 -0400

    finding vowels in a string

---
+++ js/solution026.js
@@ -0,0 +1,13 @@
+let str = "sauce"
+let counter = 0
+for(let i = 0;i < str.length;i++){
+    if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u' ){
+        counter += 1
+    }
+}
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
