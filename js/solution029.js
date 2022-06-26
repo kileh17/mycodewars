5b45266b974d3e5b7f60f855834b42eaf33c1d2c
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-06-25 20:04:50 -0400

    return negative number

---
+++ js/solution029.js
@@ -0,0 +1,7 @@
+let num = 5
+
+let answer = (Math.abs(num)) * -1
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
