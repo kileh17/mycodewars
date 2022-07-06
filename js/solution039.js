2324ee6af8c43d55ee13c870ec69969a905daec7
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-07-05 23:14:30 -0400

    boolean to string

---
+++ js/solution039.js
@@ -0,0 +1,9 @@
+function boolToWord( bool ){
+  if(bool == true){
+    return "Yes"
+  }else if(bool == false){
+    return "No"
+  }
+}
+console.log(boolToWord(false))
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
