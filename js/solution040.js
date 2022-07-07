1bd6fe45b7371becc72e8315753d713f8628eaaf
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-07-06 23:55:36 -0400

    num to string

---
+++ js/solution040.js
@@ -0,0 +1,10 @@
+let num = 5
+let text = num.toString()
+console.log(text)
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
