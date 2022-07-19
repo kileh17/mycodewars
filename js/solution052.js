93673c7015dccfe3b43cfdc321386801eff9db2f
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-07-18 22:31:56 -0400

    summation from single num

---
+++ js/solution052.js
@@ -0,0 +1,17 @@
+let num = 8
+let arr = []
+for(let i = 1; i <= num; i++){
+    arr.push(i)
+}
+console.log( arr.reduce((p,c) => p + c,0 ) )
+
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
