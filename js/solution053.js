72b8ade7f877d0215d00bb7daca0a08b4d8bb53a
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-07-20 19:10:46 -0400

    sort integer into descending order

---
+++ js/solution053.js
@@ -0,0 +1,14 @@
+let n = 42145
+
+let numArr = n.toString().split('')
+let answer = numArr.sort((a,b) => b-a).join('')
+console.log(parseInt(answer))
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
