566deae58fcd4819a8cf86990ce455f200b80952
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-07-28 22:59:28 -0400

    building Math.min function

---
+++ js/solution059.js
@@ -0,0 +1,16 @@
+// The Math.min function has stopped working, so we have to use our own function. We are off to a good start, but this function doesn't seem to handle everything properly. Add in the proper checks to return NaN for anything that isn't an actual number, except treat null like 0.
+
+// Note: This min function need not handle more than two arguments.
+
+function min(a, b){
+    a = a === null ? 0 : a;
+    b = b === null ? 0 : b;
+    
+    if(isNaN(a) || isNaN(b)){
+        return NaN
+    }else{
+        return (a<b)?a:b;
+    }
+    
+}
+
