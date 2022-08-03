4d74c03378f164cc6c804752c6f4dc2478c87ea7
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-08-02 20:29:34 -0400

    while loop to find next perf SQ

---
+++ js/solution064.js
@@ -0,0 +1,27 @@
+let sq = 1152
+
+if(Number.isInteger(Math.sqrt(sq))){
+    sq += 1
+    while(Math.sqrt(sq) % 1 != 0){
+        sq++;
+    }
+   console.log(sq)
+}else{
+    console.log(-1)
+}
+
+
+// need to while loop to return false when a number is perfect square
+
+
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+// Examples:(Input --> Output)
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
