814013186eea5ebb5f34cdceed2ed705ea795f99
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-08-09 21:27:23 -0400

    even or odd

---
+++ js/solution070.js
@@ -0,0 +1,10 @@
+let num = 7
+if(num % 2 == 0){
+    console.log("even")
+}else{
+    console.log("odd")
+}
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// MATHEMATICSFUNDAMENTALS
