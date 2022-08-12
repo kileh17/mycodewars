d0eecb2ce45f1d2714c3d3b0c539c08da719b6f8
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-08-11 20:26:51 -0400

    father son math problem

---
+++ js/solution072.js
@@ -0,0 +1,13 @@
+let sonYearsOld = 7
+let dadYearsOld = 36
+let sonAgeDouble = sonYearsOld * 2
+
+let difference = sonYearsDouble - dadYearsOld
+let answer = Math.abs(difference)
+console.log(answer)
+
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
