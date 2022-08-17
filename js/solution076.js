6d573fbdc8c4bffed5a013ce0f71c530133dcdfb
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-08-16 21:09:32 -0400

    convert boolean to string

---
+++ js/solution076.js
@@ -0,0 +1,7 @@
+let b = true
+let bString = `${b}`
+console.log(bString)
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
