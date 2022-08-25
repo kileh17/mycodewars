f475692be5f595b3e1b603d3bae3c441700b6f22
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-08-24 22:23:42 -0400

    solution082.js

---
+++ js/solution082.js
@@ -0,0 +1,16 @@
+let name = 'sam harris'
+let nameSplit = name.split(" ")
+let first = nameSplit[0]
+let last = nameSplit[1]
+let initials = `${first[0]}.${last[0]}`
+console.log(initials.toUpperCase())
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
