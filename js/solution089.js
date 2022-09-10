86a6c1b4ac7809e6dfac5173c2ba166872b4e9da
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-09-09 22:21:58 -0400

    filling an array

---
+++ js/solution089.js
@@ -0,0 +1,13 @@
+const arr = []
+const n = 5
+for(let i = 0; i < n; i++){
+    arr.push(i)
+}
+console.log(arr)
+// We want an array, but not just any old array, an array with contents!
+
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
+// arr(5) // => [0,1,2,3,4]
