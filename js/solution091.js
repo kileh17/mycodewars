462792088021e6d6b1665a432c543667f78ecf77
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-09-11 19:13:33 -0400

    easy function

---
+++ js/solution091.js
@@ -0,0 +1,17 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    let total = 0
+    total += (laLigaGoals + copaDelReyGoals + championsLeagueGoals)
+    console.log(total)
+  }
+
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
