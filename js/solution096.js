f008a0df8ce0ff94bf6286c2a786ec651671254a
kileh17 <102686098+kileh17@users.noreply.github.com>
2022-09-19 21:59:28 -0400

    average an array

---
+++ js/solution096.js
@@ -0,0 +1,10 @@
+let array = [1,2,3,4,5]
+let counter = 0
+array.forEach(element => {
+    counter += element
+
+})
+console.log(counter/array.length)
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
